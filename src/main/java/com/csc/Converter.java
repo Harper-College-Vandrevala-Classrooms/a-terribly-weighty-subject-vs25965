6ec1e50b1b974766b4747cdc5749ee4ac4de870e
vs25965 <vs25965@mail.harpercollege.edu>
2024-08-22 20:22:07 -0500

    New changes made

--- src/main/java/com/csc/Converter.java
+++ src/main/java/com/csc/Converter.java
@@ -3,10 +3,34 @@ package com.csc;
 public class Converter {
 
   public String toPounds(int ounces) {
-    throw new UnsupportedOperationException("Unimplemented method 'toPounds'");
+   
+    public static final int OUNCES_LB = 16;
+    double pounds = (ounces/OUNCES_LB);
+    if (pounds = 1)
+    {
+        System.out.printfln("%.4f lb",pounds);
+    }
+    else  
+    {
+       System.out.printfln("%.4f lbs",pounds);
+    }
+        
   }
 
   public String toPoundsAndOunces(int ounces) {
-    throw new UnsupportedOperationException("Unimplemented method 'toPoundsAndOunces'");
+
+     public static final int OUNCES_LB = 16;
+     int pounds = Math.round(ounces/OUNCES_LB);
+     int ouncesout = (ounces%OUNCES_LB);
+
+     if (pounds = 1)
+     {
+       System.out.println(" %d%n lb %d%n oz",pounds,OUNCES_LB);
+     }
+     else 
+     {
+       System.out.println(" %d%n lbs %d%n oz",pounds,OUNCES_LB);
+     }
+
   }
 }
