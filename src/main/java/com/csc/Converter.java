4c51aa6cb82e49d99a53c7cd12d9f8b050d341d1
vs25965 <vs25965@mail.harpercollege.edu>
2024-09-01 21:33:16 -0500

    New changes made
    I got 6/10. So, I made some changes to fix the problems that were possibly making the program crash.

--- src/main/java/com/csc/Converter.java
+++ src/main/java/com/csc/Converter.java
@@ -1,36 +1,48 @@
+
 package com.csc;
+import java.io.*;
 
 public class Converter {
+ 
+    public String toPounds (int ounces) {
 
-  public String toPounds(int ounces) {
-   
-    public static final int OUNCES_LB = 16;
-    double pounds = (ounces/OUNCES_LB);
-    if (pounds = 1)
-    {
-        System.out.printfln("%.4f lb",pounds);
+      String output;
+      String output2;
+      double OUNCES_LB = 16;
+      double pounds = (ounces/OUNCES_LB);
+       if (pounds == 1)
+      {
+         output = String.format("%.4f",pounds) + " lb";
+          return output;
+          
+      }
+      else  
+      {
+         output2 = String.format("%.4f",pounds) + " lb"; 
+         return output2;
+      }  
     }
-    else  
-    {
-       System.out.printfln("%.4f lbs",pounds);
-    }
-        
+    
+    
+    public String toPoundsAndOunces(int ounces) {
+
+      String output1;
+      String output2;
+      int Oz_Lb = 16;
+      int pounds1 = Math.round(ounces/Oz_Lb);
+      int ouncesout = (ounces%Oz_Lb);
+ 
+      if (pounds1 == 1)
+      {
+        output1 = pounds1 + " lb  " + ouncesout + " oz"; 
+        return output1;
+      }
+      else 
+      {
+        output2 = pounds1 + " lbs  " + ouncesout + " oz"; 
+        return output2;
+      }
+   }
+
   }
 
-  public String toPoundsAndOunces(int ounces) {
-
-     public static final int OUNCES_LB = 16;
-     int pounds = Math.round(ounces/OUNCES_LB);
-     int ouncesout = ((ounces%OUNCES_LB)*OUNCES_LB);
-
-     if (pounds = 1)
-     {
-       System.out.println(" %d%n lb %d%n oz",pounds,ouncesout);
-     }
-     else 
-     {
-       System.out.println(" %d%n lbs %d%n oz",pounds,ouncesout);
-     }
-
-  }
-}
